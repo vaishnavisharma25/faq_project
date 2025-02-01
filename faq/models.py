c3f7961e07d5264bdfa2af0b8a1c7ba7b2e431a6
vaishnavisharma25 <122250047+vaishnavisharma25@users.noreply.github.com>
2025-02-01 22:23:43 +0530

    adding depecdencies and model for faq

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from googletrans import Translator
+from django.core.cache import cache
 
-# Create your models here.
+LANGUAGES = {
+    'en': 'English',
+    'hi': 'Hindi',
+    'bn': 'Bengali',
+}
+
+class FAQ(models.Model):
+    question = models.TextField()
+    answer = RichTextField()
+    question_hi = models.TextField(blank=True, null=True)
+    question_bn = models.TextField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        translator = Translator()
+        for lang_code in LANGUAGES:
+            if lang_code != 'en':
+                translated_text = translator.translate(self.question, dest=lang_code).text
+                setattr(self, f'question_{lang_code}', translated_text)
+        super().save(*args, **kwargs)
+
+    def get_translated_question(self, lang='en'):
+        cache_key = f'faq_{self.id}_{lang}'
+        question = cache.get(cache_key)
+        if not question:
+            question = getattr(self, f'question_{lang}', self.question)
+            cache.set(cache_key, question, timeout=3600)
+        return question
+
+    def _str_(self):
+        return self.question
--- faq_project/settings.py
+++ faq_project/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework', 
+    'ckeditor', 
+    'faq',
 ]
+
+CACHES = { 'default': { 
+    'BACKEND': 'django_redis.cache.RedisCache', 
+    'LOCATION': 'redis://127.0.0.1:6379/1', 
+    'OPTIONS': { 
+        'CLIENT_CLASS': 'django_redis.client.DefaultClient', 
+            } 
+        } 
+    }
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
