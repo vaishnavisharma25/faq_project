97d68b91e607516a817b2e523f9a8a18fdd152a0
vaishnavisharma25 <122250047+vaishnavisharma25@users.noreply.github.com>
2025-02-01 23:36:56 +0530

    adding initial tests for the app

--- faq/tests.py
+++ faq/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from .models import FAQ
 
-# Create your tests here.
+class FAQTests(APITestCase):
+    def setUp(self):
+        FAQ.objects.create(question="What is Django?", answer="A web framework.")
+
+    def test_faq_translation(self):
+        response = self.client.get(reverse('faq-list') + '?lang=hi')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('translated_question', response.data[0])
+
+    def test_fallback_to_english(self):
+        response = self.client.get(reverse('faq-list') + '?lang=fr')
+        self.assertEqual(response.data[0]['translated_question'], "What is Django?")
